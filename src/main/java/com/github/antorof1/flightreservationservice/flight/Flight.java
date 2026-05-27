f49f7dcc1d459f28505d0312a79743fd7f78e8f6
Antorof1 <antorof@proton.me>
2026-05-27 14:44:31 +0200

    Add Flight entity

---
+++ src/main/java/com/github/antorof1/flightreservationservice/flight/Flight.java
@@ -0,0 +1,88 @@
+package com.github.antorof1.flightreservationservice.flight;
+
+import jakarta.persistence.*;
+
+import java.time.OffsetDateTime;
+
+@Entity
+@Table(name = "flights")
+public class Flight {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String flightNumber;
+
+    @Column(nullable = false)
+    private String departureAirport;
+
+    @Column(nullable = false)
+    private String arrivalAirport;
+
+    @Column(nullable = false)
+    private OffsetDateTime departureTime;
+
+    @Column(nullable = false)
+    private OffsetDateTime arrivalTime;
+
+    public Flight() {
+    }
+
+    public Flight(String flightNumber, String departureAirport, String arrivalAirport, OffsetDateTime departureTime,
+                  OffsetDateTime arrivalTime) {
+        this.flightNumber = flightNumber;
+        this.departureAirport = departureAirport;
+        this.arrivalAirport = arrivalAirport;
+        this.departureTime = departureTime;
+        this.arrivalTime = arrivalTime;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFlightNumber() {
+        return flightNumber;
+    }
+
+    public void setFlightNumber(String flightNumber) {
+        this.flightNumber = flightNumber;
+    }
+
+    public String getDepartureAirport() {
+        return departureAirport;
+    }
+
+    public void setDepartureAirport(String departureAirport) {
+        this.departureAirport = departureAirport;
+    }
+
+    public String getArrivalAirport() {
+        return arrivalAirport;
+    }
+
+    public void setArrivalAirport(String arrivalAirport) {
+        this.arrivalAirport = arrivalAirport;
+    }
+
+    public OffsetDateTime getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(OffsetDateTime departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public OffsetDateTime getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public void setArrivalTime(OffsetDateTime arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+}
