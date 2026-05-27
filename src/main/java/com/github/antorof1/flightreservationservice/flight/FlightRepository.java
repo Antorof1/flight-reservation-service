6402b6c770d34cac2d2ae2c9a3603ded91bf8f73
Antorof1 <antorof@proton.me>
2026-05-27 15:31:34 +0200

    Add Flight, Reservation and User repositories

---
+++ src/main/java/com/github/antorof1/flightreservationservice/flight/FlightRepository.java
@@ -0,0 +1,6 @@
+package com.github.antorof1.flightreservationservice.flight;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FlightRepository extends JpaRepository<Flight, Long> {
+}
