3bc4881142c2f0fa894bef6fd357ce87f41b5468
Antorof1 <antorof@proton.me>
2026-05-27 19:04:38 +0200

    Add flight number existence check to FlightRepository

--- src/main/java/com/github/antorof1/flightreservationservice/flight/FlightRepository.java
+++ src/main/java/com/github/antorof1/flightreservationservice/flight/FlightRepository.java
@@ -3,4 +3,5 @@ package com.github.antorof1.flightreservationservice.flight;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FlightRepository extends JpaRepository<Flight, Long> {
+    boolean existsByFlightNumber(String flightNumber);
 }
