84b70aca372659c19658a6fe2d485ead4f2b7f9e
Antorof1 <antorof@proton.me>
2026-05-27 19:15:22 +0200

    Add seat existence check to SeatRepository

--- src/main/java/com/github/antorof1/flightreservationservice/seat/SeatRepository.java
+++ src/main/java/com/github/antorof1/flightreservationservice/seat/SeatRepository.java
@@ -1,8 +1,10 @@
 package com.github.antorof1.flightreservationservice.seat;
 
+import com.github.antorof1.flightreservationservice.flight.Flight;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SeatRepository extends JpaRepository<Seat, Long> {
+    boolean existsByFlightAndSeatNumber(Flight flight, String seatNumber);
 }
