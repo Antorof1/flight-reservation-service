9a08ab9cf350c807b62569764474a7d7c15f80b6
Antorof1 <antorof@proton.me>
2026-05-31 21:00:08 +0200

    Replace default test with AbstractIntegrationTest base class

---
+++ src/test/java/com/github/antorof1/flightreservationservice/AbstractIntegrationTest.java
@@ -0,0 +1,48 @@
+package com.github.antorof1.flightreservationservice;
+
+import com.github.antorof1.flightreservationservice.flight.FlightRepository;
+import com.github.antorof1.flightreservationservice.reservation.ReservationRepository;
+import com.github.antorof1.flightreservationservice.seat.SeatRepository;
+import com.github.antorof1.flightreservationservice.user.UserRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.redis.core.RedisCallback;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Import(TestcontainersConfiguration.class)
+public abstract class AbstractIntegrationTest {
+    @Autowired
+    protected WebTestClient testClient;
+
+    @Autowired
+    protected UserRepository userRepository;
+
+    @Autowired
+    protected FlightRepository flightRepository;
+
+    @Autowired
+    protected SeatRepository seatRepository;
+
+    @Autowired
+    protected ReservationRepository reservationRepository;
+
+    @Autowired
+    protected StringRedisTemplate redisTemplate;
+
+    @AfterEach
+    void clearDatabase() {
+        reservationRepository.deleteAllInBatch();
+        seatRepository.deleteAllInBatch();
+        flightRepository.deleteAllInBatch();
+        userRepository.deleteAllInBatch();
+
+        redisTemplate.execute((RedisCallback<Void>) connection -> {
+            connection.serverCommands().flushAll();
+            return null;
+        });
+    }
+}
