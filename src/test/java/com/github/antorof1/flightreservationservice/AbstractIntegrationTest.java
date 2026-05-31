22f7fb2da154135e42f0257097b1c5399d10c812
Antorof1 <antorof@proton.me>
2026-05-31 21:52:00 +0200

    Configure WebTestClient for integration testing

--- src/test/java/com/github/antorof1/flightreservationservice/AbstractIntegrationTest.java
+++ src/test/java/com/github/antorof1/flightreservationservice/AbstractIntegrationTest.java
@@ -8,12 +8,14 @@ import com.github.antorof1.flightreservationservice.user.UserRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webtestclient.autoconfigure.AutoConfigureWebTestClient;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
 @Import(TestcontainersConfiguration.class)
 public abstract class AbstractIntegrationTest {
     @Autowired
