8624c4b5271a023f6c448e72e8dacb80c4198b3a
Antorof1 <antorof@proton.me>
2026-07-02 22:55:45 +0200

    Configure AuthenticationManager bean in SecurityConfig

--- src/main/java/com/github/antorof1/flightreservationservice/config/SecurityConfig.java
+++ src/main/java/com/github/antorof1/flightreservationservice/config/SecurityConfig.java
@@ -4,6 +4,8 @@ import com.github.antorof1.flightreservationservice.security.JwtAuthenticationFi
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -41,5 +43,11 @@ public class SecurityConfig {
                 .anyRequest().authenticated())
             .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
             .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+        throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
