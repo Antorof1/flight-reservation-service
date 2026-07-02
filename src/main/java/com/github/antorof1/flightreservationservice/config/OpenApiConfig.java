21da742b1c304ae8e0ab9c7e19f8cbeb59ae3182
Antorof1 <antorof@proton.me>
2026-07-02 23:03:12 +0200

    Add JWT bearer authentication to OpenAPI configuration

--- src/main/java/com/github/antorof1/flightreservationservice/config/OpenApiConfig.java
+++ src/main/java/com/github/antorof1/flightreservationservice/config/OpenApiConfig.java
@@ -1,9 +1,12 @@
 package com.github.antorof1.flightreservationservice.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,6 +15,13 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI flightReservationOpenAPI() {
         return new OpenAPI()
+            .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+            .components(new Components()
+                .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                    .name("bearerAuth")
+                    .type(SecurityScheme.Type.HTTP)
+                    .scheme("bearer")
+                    .bearerFormat("JWT")))
             .info(new Info()
                 .title("Flight Reservation Service API")
                 .description("API for managing flights, seats, users, and reservations.")
