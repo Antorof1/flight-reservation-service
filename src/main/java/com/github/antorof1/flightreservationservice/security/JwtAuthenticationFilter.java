c6fad2391c9c5111b6e001aa4431b69358224828
Antorof1 <antorof@proton.me>
2026-07-03 15:37:19 +0200

    Skip JWT authentication filter for auth endpoints

--- src/main/java/com/github/antorof1/flightreservationservice/security/JwtAuthenticationFilter.java
+++ src/main/java/com/github/antorof1/flightreservationservice/security/JwtAuthenticationFilter.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 
@@ -21,11 +22,17 @@ import java.util.List;
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final JwtUtils jwtUtils;
     private final HandlerExceptionResolver resolver;
+    private final AntPathMatcher pathMatcher = new AntPathMatcher();
 
     public JwtAuthenticationFilter(JwtUtils jwtUtils,
                                    @Qualifier("handlerExceptionResolver") HandlerExceptionResolver resolver) {
         this.jwtUtils = jwtUtils;
         this.resolver = resolver;
+    }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) {
+        return pathMatcher.match("/api/v1/auth/**", request.getServletPath());
     }
 
     @Override
