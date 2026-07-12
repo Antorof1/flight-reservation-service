a7d02c3f225fa1d0c4298af5b65f6985899ad869
Antorof1 <antorof@proton.me>
2026-07-12 22:14:02 +0200

    Permit unauthenticated access to Prometheus actuator endpoint

--- src/main/java/com/github/antorof1/flightreservationservice/config/SecurityConfig.java
+++ src/main/java/com/github/antorof1/flightreservationservice/config/SecurityConfig.java
@@ -44,6 +44,7 @@ public class SecurityConfig {
                 .requestMatchers(HttpMethod.GET, "/api/v1/flights/**").permitAll()
                 .requestMatchers("/api/v1/auth/**").permitAll()
                 .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                .requestMatchers("/actuator/prometheus").permitAll()
                 .anyRequest().authenticated())
             .exceptionHandling(exceptionHandling -> exceptionHandling.authenticationEntryPoint(authenticationEntryPoint))
             .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
